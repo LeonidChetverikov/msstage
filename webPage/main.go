3802a1512de6e7651e4a2c2978da699080ee28a0
Leonid Chetverikov <leonid.chetverikov@eleks.com>
2022-02-09 16:15:41 +0200

    Add webpage

---
+++ webPage/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"net/http"
+)
+
+func main() {
+	// The "HandleFunc" method accepts a path and a function as arguments
+	http.HandleFunc("/", handler)
+
+	http.ListenAndServe(":80", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Hello MSStage!")
+}
